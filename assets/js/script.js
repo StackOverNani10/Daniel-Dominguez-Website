880213dc853b6e957615f63cf383e0863b105e8d
StackOverNani <1097711@est.intec.edu.do>
2024-01-03 11:14:45 -0400

    - Update CV and Map View

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,29 +101,3 @@ sr.reveal(`.home__data`, { origin: 'bottom' })
 sr.reveal(`.about__data, .skills__content`, { origin: 'left' })
 sr.reveal(`.about__img`, { origin: 'right' })
 sr.reveal(`.portfolio__card`, { interval: 100 })
-
-
-/*=================== CARD 3D ===================*/
-
-// Initialize Tilt JS
-VanillaTilt.init(
-    document.querySelectorAll(".js-tilt"), {
-    max: 25,
-    speed: 400,
-}
-);
-
-//Get the li elements
-const sizes = document.querySelectorAll(".size");
-
-//Add a click event to each
-for (let i = 0; i < sizes.length; i++) {
-    sizes[i].addEventListener("click", () => {
-        //Remove active style from all
-        for (let j = 0; j < sizes.length; j++) {
-            sizes[j].classList.remove("active");
-        }
-        //Add active style to the clicked one
-        sizes[i].classList.toggle("active");
-    });
-}
